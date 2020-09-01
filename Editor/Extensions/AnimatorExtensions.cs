c9acf66cc6c22ec99d76002a7f734d672658a390
Joshuarox100 <joshuabender10@gmail.com>
2020-09-01 13:06:39 -0500

    Seperated into multiple files.

--- Editor/Extensions/AnimatorExtensions.cs
+++ Editor/Extensions/AnimatorExtensions.cs
@@ -1,134 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEditor.Animations;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace BMBLibraries
 {
-    namespace Classes
-    {
-        public class Backup
-        {
-            private string[] assets;
-            private byte[][] backup;
-
-            public Backup()
-            {
-                assets = new string[0];
-                backup = new byte[0][];
-            }
-
-            public Backup(AssetList assets)
-            {
-                this.assets = new string[assets.ToArray().Length];
-                backup = new byte[this.assets.Length][];
-                for (int i = 0; i < this.assets.Length; i++)
-                {
-                    try
-                    {
-                        this.assets[i] = assets[i].path;
-                        backup[i] = File.ReadAllBytes(assets[i].path);
-                    }
-                    catch (Exception err)
-                    {
-                        backup[i] = null;
-                        Debug.LogError(err);
-                    }
-                }
-            }
-
-            public void AddToBackup(Asset asset)
-            {
-                int index = -1;
-                for (int i = 0; i < assets.Length; i++)
-                {
-                    if (assets[i] == asset.path)
-                    {
-                        index = i;
-                        break;
-                    }
-                }
-
-                if (index != -1)
-                {
-                    try
-                    {
-                        backup[index] = File.ReadAllBytes(asset.path);
-                    }                    
-                    catch (Exception err)
-                    {
-                        backup[index] = null;
-                        Debug.LogError(err);
-                    }
-                }
-                else
-                {
-                    string[] newAssets = new string[assets.Length + 1];
-                    assets.CopyTo(newAssets, 1);
-                    newAssets[0] = asset.path;
-                    assets = newAssets;
-
-                    byte[][] newBytes = new byte[backup.Length + 1][];
-                    backup.CopyTo(newBytes, 1);
-                    try
-                    {
-                        newBytes[0] = File.ReadAllBytes(asset.path);
-                    }
-                    catch (Exception err)
-                    {
-                        newBytes[0] = null;
-                        Debug.LogError(err);
-                    }
-                    backup = newBytes;
-                }               
-            }
-
-            public bool RestoreAssets()
-            {
-                for (int i = 0; i < assets.Length; i++)
-                {
-                    try
-                    {
-                        if (backup[i] == null)
-                        {
-                            return false;
-                        }
-                        else
-                        {
-                            if (!File.Exists(assets[i]))
-                            {
-                                File.Create(assets[i]);
-                            }
-                            File.WriteAllBytes(assets[i], backup[i]);
-                        }
-                    }
-                    catch (Exception err)
-                    {
-                        Debug.LogError(err);
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-
-        public class ExtraMath
-        {
-            public static IEnumerable<IEnumerable<T>> GetPowerSet<T>(IEnumerable<T> input)
-            {
-                var seed = new List<IEnumerable<T>>() { Enumerable.Empty<T>() }
-                  as IEnumerable<IEnumerable<T>>;
-
-                return input.Aggregate(seed, (a, b) =>
-                  a.Concat(a.Select(x => x.Concat(new List<T>() { b }))));
-            }
-        }
-    }    
-
     namespace Extensions
     {
         public static class AnimatorExtensions
@@ -420,5 +295,5 @@ namespace BMBLibraries
                 }
             }
         }
-    }   
+    }
 }
